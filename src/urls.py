1b7ad1ad6193f08243b5bcd3b86f7f53eca4021d
ModernTao <1152232298@qq.com>
2019-05-24 00:09:17 +0700

    modified project path

--- src/urls.py
+++ src/urls.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #coding=utf-8
-"""testpro URL Configuration
+"""src URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
--- src/wsgi.py
+++ src/wsgi.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #coding=utf-8
 """
-WSGI config for testpro project.
+WSGI config for src project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -13,6 +13,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testpro.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.settings")
 
 application = get_wsgi_application()
